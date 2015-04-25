6d5ced6f1f39516aa5ce20c0b2170424bfe5b929
Ivan Elenskiy <elenskivan@hotmail.com>
2015-04-25 17:20:45 +0300

    Final lab - final commit

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
@@ -6,28 +8,69 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.ExecutionException;
+import java.util.List;
+import java.util.Vector;
 
 /**
  * Created by Student on 23.04.2015.
  */
-public class ChatServer {
+public class ChatServer{
     private JPanel Panel;
     private JButton Button;
     private JList UserList;
+    private DefaultListModel listModel;
+    private static List<Socket> socketList;
+    private static int last = 0;
 
     public ChatServer() {
+        listModel = new DefaultListModel();
+        socketList = new Vector();
+        UserList.setModel(listModel);
+        UserList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        UserList.setVisibleRowCount(8);
+        Button.setEnabled(false);
         Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                int index = UserList.getSelectedIndex();
+                listModel.remove(index);
+                try {
+                    socketList.get(index).close();
+                }
+                catch (IOException s)
+                {System.out.println("Error: " + s);}
+                socketList.remove(index);
+                last--;
 
+                int size = listModel.getSize();
+                if (size == 0) {
+                    Button.setEnabled(false);
+                } else {
+                    if (index == listModel.getSize()) {
+                        index--;
+                    }
+                    UserList.setSelectedIndex(index);
+                    UserList.ensureIndexIsVisible(index);
+                }
+            }
+        });
+        UserList.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                if (e.getValueIsAdjusting() == false) {
+                    if (UserList.getSelectedIndex() == -1) {
+                        Button.setEnabled(false);
+                    } else {
+                        Button.setEnabled(true);
+                    }
+                }
             }
         });
     }
 
-    public void SetUserList(String[] userList)
+    public void AddNewElement(String msg)
     {
-        UserList = new JList(userList);
+        listModel.addElement(msg);
     }
 
     public static void main(String[] args) throws Exception {
@@ -37,17 +80,17 @@ public class ChatServer {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
-        Socket socket = null;
-        try {
-            ServerSocket serverSocket = new ServerSocket(27015);
-            socket = serverSocket.accept();
-            BufferedReader msg = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            //chatServer.SetUserList(socket.getInetAddress().toString() + " " + msg.readLine());
-            socket.close();
-        }
-        catch (IOException e)
-        {
-            System.out.println("Error: " + e);
+        boolean done = false;
+        ServerSocket serverSocket = new ServerSocket(27015);
+        while (!done) {
+            try {
+                socketList.add(serverSocket.accept());
+                last = socketList.size() - 1;
+                BufferedReader msg = new BufferedReader(new InputStreamReader(socketList.get(last).getInputStream()));
+                chatServer.AddNewElement(socketList.get(last).getInetAddress().toString() + " " + msg.readLine());
+            } catch (IOException e) {
+                System.out.println("Error: " + e);
+            }
         }
     }
 }
